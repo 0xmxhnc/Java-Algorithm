85ffda0f9b2495f43026c103468d4a215cc937b2
0xmxhnc <RobertKai@163.com>
2018-11-05 23:20:52 +0800

    完善

--- RSADemo2.java
+++ RSADemo2.java
@@ -1,5 +1,3 @@
-package rsa;
-
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,14 +6,14 @@ import java.util.Scanner;
 
 class ProducePrime {
 	/*
-	 * 获取第一个素数
+	 * 获取第一个素数的方法
 	 */
 	
 	public Integer Prime1() {
 		List<Integer> list = new ArrayList<Integer>();// 存储素数的集合
-		for (int i = (int) Math.pow(2, 16); i <= Math.pow(2, 17); i++) {
+		for (int i = (int) Math.pow(2, 16) + 1; i <= Math.pow(2, 17); i+=2) {// 判断奇数
 			boolean num2 = true;// 防止被false覆盖
-			for (int m = 2; m < Math.sqrt(i); m++) {
+			for (int m = 2; m <= Math.sqrt(i); m++) {// 提高试除效率
 				if (i % m == 0) {
 					num2 = false;// 不是素数
 					break;
@@ -32,7 +30,7 @@ class ProducePrime {
 	}
 
 	/*
-	 * 获取第二个素数
+	 * 获取第二个素数的方法
 	 */
 	
 	public int Prime2() {
@@ -42,7 +40,7 @@ class ProducePrime {
 		int num = new Random().nextInt(max - min + 1) + min;// 随机获取指定范围内的数字
 		if (Odd(num)) {
 			boolean flag = true;
-			for (int m = 2; m < Math.sqrt(num); m++) {
+			for (int m = 2; m <= Math.sqrt(num); m++) {// 提高试除效率
 				if (num % m == 0) {
 					flag = false;// 不是素数
 					num = num + 2;
@@ -56,7 +54,7 @@ class ProducePrime {
 		else {
 			num = num + 1;
 			boolean flag = true;
-			for (int m = 2; m < Math.sqrt(num); m++) {
+			for (int m = 2; m <= Math.sqrt(num); m++) {// 提高试除效率
 				if (num % m == 0) {
 					flag = false;// 不是素数
 					num = num + 2;
@@ -85,12 +83,13 @@ class ProducePrime {
 
 class Division {
 	public void divison(int m) {
-		int temp = 0;
-		int c = 0;
+		int temp = 1;
+		int c = 1;
 		int[] arr = new int[1000];
-		for (int i = 2; i < 1000; i++) {
+		arr[0] = 2;
+		for (int i = 3; i < 1000; i+=2) {// 判断奇数即可
 			boolean flag = true;
-			for (int q = 2; q < i; q++) {
+			for (int q = 2; q <= Math.sqrt(i); q++) {
 				if (i % q == 0) {
 					flag = false;// 该数不是素数
 					break;
@@ -101,66 +100,89 @@ class Division {
 				temp++;
 			}
 		}
-		System.out.println("试除开始:");
+		
+		System.out.println("1000以内素数试除开始:");
+		System.out.println();
+		
 		for (int a = 0; a < temp; a++) {
-			if (m > arr[a]) {
-				int b = m % arr[a];// 试除1000以内的素数,保存余数
-				c++;
-				System.out.println("试除第" + c + "次:" + m + " % " + arr[a] + "=" + b);
-				System.out.println("得到的余数为:" + b);
-				System.out.println();
-				if (c == 168) {
-					System.out.println("试除结束");
-				}
-			}
+			int b = m % arr[a];// 试除1000以内的素数,保存余数
+			System.out.println("试除第" + c + "次:" + m + " % " + arr[a] + "=" + b);
+			c++;
+			System.out.println("得到的余数为:" + b);
+			System.out.println();
 		}
+		System.out.println("1000以内素数试除结束");
 	}
 }
 
 class MillerRabin {
 	/*
-	 * n为测试数据，t
+	 * n为检测数据
 	 */
 	
-	public static boolean MillerRabin2(int n, int t) {
-		long s = 0;
-		for(s = 1; s <= n; s++) {
-			if((n - 1) == Math.pow(2, s) * t) {
-				if((Math.pow(b, t) % n == 1) || ((Math.pow(b, t) % n) - n) == -1) {
-					
+	public boolean MillerRabin2(int n) {
+		int a = new Random().nextInt(n) + 1;
+		long s = 1;
+		long n2 = n - 1;
+		long n3 = n - 1;
+		int t = 0, temp = 0;
+		boolean flag = true;
+		out :
+			for(;;) {
+			if (n2 % 2 != 0) {
+				s = temp;
+				break out;
+			}else {
+				n2 = n2 / 2;
+				temp++;
+			}
+		}
+		t = (int) (n3 / Math.pow(2, s));
+		int b = (int) Math.pow(a, t);
+		for(long i = (long) Math.pow(2,s - 1); i >= 1; i--){
+			if((Math.pow(b, i) + 1) % n == 0){
+				break;
+			}
+			if(i == 1){
+				if((Math.pow(b, i) - 1) % n == 0){
+					break;
+				}
+				else{
+					flag = false;
 				}
 			}
 		}
+		return flag;
 	}
 
 }
 
 class EuclideanAlgorithm {
 	/*
-	 * 获取私钥d
+	 * 获取私钥d的方法
 	 * 欧几里得扩展算法
 	 * 公钥e = 17
 	 */
 	
-	long x, y;
-	public long exgcd(long a, long b) {
-		if (b == 0) {
-			x = 1;
-			y = 0;
+	BigInteger x, y, a, b, t, r;
+	public BigInteger exgcd(BigInteger a, BigInteger b) {
+		if (b == BigInteger.valueOf(0)) {
+			x = BigInteger.valueOf(1);
+			y = BigInteger.valueOf(0);
 			return a;
 		}
-		long r = exgcd(b, a % b);
-		long t = y;
-		y = x - (a / b) * y;
-		x = t;
+		r = exgcd(b, a.mod(b));
+		t = x;
+		x = y;
+		y = t.subtract((a.divide(b)).multiply(y));
 		return r;
 	}
 	
-	public void XY(long fn, long e) {
+	public void XY(BigInteger fn, BigInteger e) {
 		System.out.println("解得x为:" + x);
 		System.out.println("解得y为:" + y);
-		if (y < 0) {
-			System.out.println("所以密钥d为:" + (y + fn));
+		if (y.compareTo(BigInteger.valueOf(0)) < 0) {
+			System.out.println("所以密钥d为:" + (y.add(fn)));
 		}else {
 			System.out.println("所以密钥d为:" + y);
 		}
@@ -169,17 +191,17 @@ class EuclideanAlgorithm {
 
 }
 
-class MoChongFuPinFang {
+class MoChongFuPingFang {
 	/*
 	 * 加密
-	 * 加密明文m = 32655，公钥e = 17，模数n
+	 * 输入加密明文m = 32655，公钥e = 17，模数为n
 	 */
 	
 	String binary;
 	long a = 1;
 	BigInteger a2 = BigInteger.valueOf(a);
 
-	public BigInteger ChongFuPinFang(long m, long e2, long n) {
+	public BigInteger Encryption(long m, long e2, long n) {
 		binary = Long.toBinaryString(e2);
 		BigInteger a3 = a2;
 		BigInteger m2 = BigInteger.valueOf(m);
@@ -202,7 +224,7 @@ class MoChongFuPinFang {
 
 }
 
-class PinFangChen {
+class PingFangCheng {
 	/*
 	 * 解密
 	 * 密文m，私钥d，模数n
@@ -212,7 +234,7 @@ class PinFangChen {
 	long x = 0, y = 1;
 	BigInteger x2 = BigInteger.valueOf(x);
 	BigInteger y2 = BigInteger.valueOf(y);
-	public BigInteger JieMi(long m, long d, long n) {
+	public BigInteger Decryption(long m, long d, long n) {
 		binary = Long.toBinaryString(d);
 		BigInteger x3 = x2;
 		BigInteger m2 = BigInteger.valueOf(m);
@@ -242,16 +264,18 @@ public class RSADemo {
 		ProducePrime ProducePrime = new ProducePrime();
 		Division Division = new Division();
 		EuclideanAlgorithm euclideanAlgorithm = new EuclideanAlgorithm();
-		MoChongFuPinFang moChongFuPinFang = new MoChongFuPinFang();
-		PinFangChen pinFangChen = new PinFangChen();
+		MoChongFuPingFang moChongFuPingFang = new MoChongFuPingFang();
+		PingFangCheng pingFangCheng = new PingFangCheng();
 		MillerRabin MillerRabin = new MillerRabin();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("/****************/");
 		System.out.println("RSA算法加密解密实现");
 		System.out.println("/****************/");
-		int p = ProducePrime.Prime1();
+		int p = 1009;//ProducePrime.Prime1();
+		//int p = ProducePrime.Prime1();
 		System.out.println("输出满足要求的第一个素数p:" + p);
-		int q = ProducePrime.Prime2();
+		int q = 997;//ProducePrime.Prime2();
+		//int q = ProducePrime.Prime2();
 		System.out.println("输出满足要求的第二个素数q:" + q);
 		System.out.println("/****************/");
 		long n = (long)p * q;//避免越界，强制转型
@@ -261,27 +285,31 @@ public class RSADemo {
 		System.out.println("/****************/");
 		Division.divison(p);
 		System.out.println("/****************/");
-		boolean flag = MillerRabin.MillerRabin2((int) 9, 5);
-		System.out.println(flag);
+		int p2 = (int)p;
+		boolean flag = MillerRabin.MillerRabin2(p2);
+		System.out.println("Miller-Rabin算法检测标志符为:" + flag);
 		System.out.println("/****************/");
 		System.out.println("请输入公钥e:");
-		long e = scanner.nextInt();
-		System.out.println("公钥e与fn的最大公约数为:" + euclideanAlgorithm.exgcd(fn, e));//最大公约数		
-		euclideanAlgorithm.XY(fn, e);
+		long e = scanner.nextLong();
+		BigInteger e3 = BigInteger.valueOf(e);
+		BigInteger fn2 = BigInteger.valueOf(fn);
+		System.out.println("公钥e与fn的最大公约数为:" + euclideanAlgorithm.exgcd(fn2, e3));//最大公约数		
+		euclideanAlgorithm.XY(fn2, e3);
 		System.out.println("/****************/");
 		//加密明文
+		System.out.println("加密开始:");
 		System.out.println("请输入明文m:");
 		long m = scanner.nextLong();
 		System.out.println("请输入公钥e:");
-		long e2 = scanner.nextInt();
-		System.out.println("则加密后的密文m2为:" + moChongFuPinFang.ChongFuPinFang(m, e2, n));
+		long e2 = scanner.nextLong();
+		System.out.println("加密后的密文m2为:" + moChongFuPingFang.Encryption(m, e2, n));
 		System.out.println("/****************/");
 		//解密密文
+		System.out.println("解密开始:");
 		System.out.println("请输入密文m2:");
 		long m2 = scanner.nextLong();
 		System.out.println("请输入密钥d:");
 		long d2 = scanner.nextLong();
-		System.out.println("解密后的明文为:" + pinFangChen.JieMi(m2, d2, n));
-
+		System.out.println("解密后的明文为:" + pingFangCheng.Decryption(m2, d2, n));
 	}
 }
